f00e8a76afaab18e55c144694fd9450fe25d0614
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-02-14 16:17:07 +0100

    add even or odd calculus logich

--- exercise-2/main.js
+++ exercise-2/main.js
@@ -1,8 +1,7 @@
 /* Pari e Dispari
 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.*/
 
-const userChoice = Number(prompt('scegliere 1 per pari due per dispari'));
-const userNumber = Number(prompt('scegliere un numero intero da 1 a 5'));
+
 
 
 function randomNumber() {
@@ -10,5 +9,25 @@ function randomNumber() {
     return numb;
 }
 
-const pcNumber = randomNumber(numb);
+function sum(userNumb, pcNumb){
+    let oddFlag = false;
+    let evenFlag = false;
+    const sum = userNumb + pcNumb;
+    if (sum % 2 == 0){
+        evenFlag = true;
+    } else {
+        oddFlag = true;
+    }
+    console.log(oddFlag, evenFlag);
+    
+    return evenFlag, oddFlag;
+}
+
+
+
+const userChoice = Number(prompt('scegliere 1 per pari due per dispari'));
+const userNumber = Number(prompt('scegliere un numero intero da 1 a 5'));
+const pcNumber = randomNumber();
 console.log(pcNumber);
+
+sum(userNumber, pcNumber);
