3372c173e8d0eab12ac76b3bc8e51303421954b6
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-02-14 14:51:36 +0100

    add thougth process ad variables

---
+++ main.js
@@ -0,0 +1,12 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma*/
+ //radar
+
+ function getPalindrome (word){
+    //mettere ogni carattere della parola in un array
+    //creare un array analogo ed invertirlo
+    //cofrontare i due array
+ }
+
+ const userWord = prompt('inserire una parola per scoprire se e palindroma');
+ getPalindrome(userWord);
