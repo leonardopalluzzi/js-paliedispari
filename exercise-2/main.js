df7765de4c0d7e01de319c160236fb500798c1db
leonardopalluzzi <lp.palluzzi@gmail.com>
2025-02-14 16:24:19 +0100

    add output message and winner calc

--- exercise-2/main.js
+++ exercise-2/main.js
@@ -10,24 +10,36 @@ function randomNumber() {
 }
 
 function sum(userNumb, pcNumb){
-    let oddFlag = false;
     let evenFlag = false;
     const sum = userNumb + pcNumb;
     if (sum % 2 == 0){
         evenFlag = true;
-    } else {
-        oddFlag = true;
     }
-    console.log(oddFlag, evenFlag);
+    console.log(evenFlag);
     
-    return evenFlag, oddFlag;
+    return evenFlag;
 }
 
 
 
-const userChoice = Number(prompt('scegliere 1 per pari due per dispari'));
+const userChoice = Number(prompt('scegliere 1 per pari 2 per dispari'));
 const userNumber = Number(prompt('scegliere un numero intero da 1 a 5'));
 const pcNumber = randomNumber();
+const flag = sum(userNumber, pcNumber);
 console.log(pcNumber);
 
-sum(userNumber, pcNumber);
+if(userChoice == 1 && flag == true){
+    alert('HAI VINTO! il risultato e PARI');
+    
+} else if(userChoice == 2 && flag == true) {
+    alert('HAI PERSO il risultato e DISPARI');
+    
+} else if(userChoice == 1 && flag == false){
+    alert('HAI PERSO il risultato e dispari');
+    
+} else if(userChoice ==2 && flag == false){
+    alert('HAI VINTO il risultato e DISAPRI');
+    
+}
+
+location.reload();
